5baff91812cdd2d5a6b5e8a2bf06471c49fbc9e0
Alex Market <mercadoalex@gmail.com>
2022-03-27 16:01:04 -0600

    correcion de pitch y rate

--- script.js
+++ script.js
@@ -115,8 +115,9 @@ function speak(){
         break;
       }
     } */
-    utterThis.pitch = 5;
-    utterThis.rate = 10;
+    
+    //utterThis.pitch = 5;
+    //utterThis.rate = 10;
     synth.speak(utterThis);
   }
 }
